787c4f28372fe39999f8bfc453e4852fc61c7b98
Shaun Thomas <libnexus@outlook.com>
2023-11-10 10:57:04 +0000

    Added compile info to string method for file saving, added attributes for storing returned service objects from info entry widget, removed toplevel from starting on Main creation.

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ from application.data.service import Service
 root = Tk()
 
 
+def _service_info(service: Service):
+    information = "    Name: %s\n" % service.name
+    information += "    Address (1): %s\n" % service.address_line_1
+    information += "    Address (2): %s\n" % service.address_line_2
+    if service.email:
+        information += "    Email: %s\n" % service.email
+    if service.telephone:
+        information += "    Tel: %s\n" % service.telephone
+    information += "\n"
+    return information
+
+
 class Main(Frame):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
@@ -15,14 +27,33 @@ class Main(Frame):
         if not self._jdbi.init_db():
             showerror("Uh Oh", "The database had trouble connecting. Try restarting the program.")
             raise SystemExit
-        self._service_info_entry = ServiceInformationEntry(root,
-                                                           self._jdbi.get_postcode("LL572EH"),
-                                                           "GP",
-                                                           self._jdbi,
-                                                           lambda o: print("returned", o))
         root.state("iconic")
 
+        self._gp: Service | None = None
+        self._dentist: Service | None = None
+        self._optician: Service | None = None
+        self._schools: list[Service, ...] = []
+
+        # Menu
+
         self._menu = Menu(root)
+
+    def _compile_information_to_string(self) -> str:
+        """
+        Compiles the information given into a nice string which can then be exported to a txt file
+        Modifications to this method may make it compile to html which can be converted to pdf
+        """
+        information = ""
+
+        for name, service in zip(["GP", "Dentist", "Optician"], [self._gp, self._dentist, self._optician]):
+            information += "Information for %s\n" % name
+            information += _service_info(service)
+
+        for i, school in enumerate(self._schools):
+            information += "Child %ds school:" % i
+            information += _service_info(school)
+
+        return information
 
 
 main = Main(root)
