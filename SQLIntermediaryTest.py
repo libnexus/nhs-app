a91dddb7f591d2cf52acf6b62e90e74088c8b5ad
Ricardo <r.dejesus462004@outlook.com>
2023-11-30 10:18:43 +0000

    Create SQLIntermediaryTest.py

---
+++ SQLIntermediaryTest.py
@@ -0,0 +1,24 @@
+from SQLDatabaseIntermediary import DBConnector
+import unittest
+
+class IntermediaryTest(DBConnector)
+   
+    def test_get_all_postcodes(self)
+
+
+    def test_get_postcode(self)
+        
+
+    def test_get_services(self)
+
+    def test_add_postcode(self)
+
+    def test_add_service(self)
+
+    def test_update_service(self)
+
+    def test_del_postcode(self)
+
+    def test_del_service(self)
+
+
